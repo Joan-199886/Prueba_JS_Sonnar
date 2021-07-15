f1347b11ba3bd80cab28418f3b89535fd419f75a
Joan-199886 <55800066+Joan-199886@users.noreply.github.com>
2021-07-15 16:48:15 -0500

    Update Modificar_Configuracion.js

--- Mod_Config/Modificar_Configuracion.js
+++ Mod_Config/Modificar_Configuracion.js
@@ -5,6 +5,7 @@ try {
     // `who-to-greet` input defined in action metadata file
     const url = core.getInput('files-added');
     console.log(url);
+     console.log("holamundo");
 
   } catch (error) {
     core.setFailed(error.message);
