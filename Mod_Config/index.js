8b15be1f33a32a36146d4eada9fc36b0822d613b
joan-199886 <joan-herrera@upc.edu.co>
2021-07-16 12:35:46 -0500

    holamundo

--- Mod_Config/index.js
+++ Mod_Config/index.js
@@ -10,10 +10,8 @@ try {
     const token = core.getInput('token');
     const url_config = core.getInput('url-config');
 
-
-    var secrets = JSON.parse(process.env.SECRETS);
     console.log("====================================================");
-    console.log("SECRETS"+secrets[0]);
+    
     console.log("url: "+url);
     console.log("url config: "+ url_config);
     console.log("token: "+token);
