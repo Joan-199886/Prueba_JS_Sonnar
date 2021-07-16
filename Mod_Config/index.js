f0ed4d34c6e2d5253799509ac5bc0d844de12854
joan-199886 <joan-herrera@upc.edu.co>
2021-07-16 15:20:43 -0500

    holamundo

--- Mod_Config/index.js
+++ Mod_Config/index.js
@@ -9,11 +9,14 @@ try {
     const url = core.getInput('files-added');
     const token = core.getInput('token').split('');
     const url_config = core.getInput('url-config').split('');
-   
+    
+    const url2 = url_config.join("");
+
     console.log("====================================================");
     console.log("url: "+url);
     console.log("url config: "+ url_config.join(""));
     console.log("token: "+token.join("") );
+    console.log(url2);
     
   } catch (error) {
     core.setFailed(error.message);
