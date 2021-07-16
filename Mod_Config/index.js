671018c80351bb6d775a7c06874344853ed5424d
joan-199886 <joan-herrera@upc.edu.co>
2021-07-16 14:23:21 -0500

    holamundo

--- Mod_Config/index.js
+++ Mod_Config/index.js
@@ -9,9 +9,9 @@ try {
     const url = core.getInput('files-added');
     const token = core.getInput('token');
     const url_config = core.getInput('url-config');
-
+    var secrets = JSON.parse(process.env.TOKEN);
     console.log("====================================================");
-    
+    console.log("secrets: "+ secrets)
     console.log("url: "+url);
     console.log("url config: "+ url_config);
     console.log("token: "+token);
