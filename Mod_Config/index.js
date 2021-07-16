7c5aee35900c7d1ffc7c71a9dd84f4957ad77826
joan-199886 <joan-herrera@upc.edu.co>
2021-07-16 10:45:01 -0500

    final2

--- Mod_Config/index.js
+++ Mod_Config/index.js
@@ -6,7 +6,10 @@ try {
 
     const url = core.getInput('files-added');
     const token = core.getInput('token');
+    const url_config = core.getInput('url-config');
+
     console.log("url: "+url);
+    console.log("url config: "+ url_config);
     console.log("token: "+token);
 
     const auth = createActionAuth();
