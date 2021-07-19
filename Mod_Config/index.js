88bfb8104f2962d1bff6e11e74c7a4c14e2d2e56
joan-199886 <joan-herrera@upc.edu.co>
2021-07-19 11:05:56 -0500

    modify

--- Mod_Config/index.js
+++ Mod_Config/index.js
@@ -3,23 +3,64 @@ const github = require('@actions/github');
 const { createActionAuth } = require("@octokit/auth-action");
 const { Base64 } = require("js-base64");
 
-async function Run(){
-try {
-    const url = core.getInput('files-added');
-    const repotoken = core.getInput('token');
-    const url_config = core.getInput('url-config');
-    // var branch = context.payload.pull_request.head.ref;
 
+async function overwriteFile(repoToken,pathFile)
+{
     const {payload: {repository} } = github.context;
 
     const repoFullName = repository.full_name;
 
-    console.log("RepoFullName:"+repoFullName);
-    
-    if(url_config && url)
+    if(repoFullName)
     {
-      console.log("Url_Config : "+url_config.split(""));
-      console.log("Url :"+url.split(""));
+      const [owner,repo] = repoFullName.split("/");
+
+      var master="El archivo ha sido modificado "+ url;
+
+      const octokit = github.getOctokit(repoToken);
+      const sha = await getSHA(owner,repo,pathFile);
+      const contentFile = Base64.encode(master);
+
+      const httpResult= await octokit.repos.createOrUpdateFileContents({
+        owner,
+        repo,
+        path: pathFile,
+        message: 'update master.xml',
+        content: contentFile,
+        branch: "main",
+        sha
+        }
+      );
+      console.log(httpResult);
+      return httpResult.status.toString();
+      
+    }
+    
+}
+
+async function getSHA(owner,repo,path) {
+  const repo_Token = core.getInput('token');
+  const octokit = github.getOctokit(repo_Token);
+  const result = await octokit.repos.getContent({
+    owner,
+    repo,
+    path,
+  });
+  const sha = result.data.sha;
+  return sha;
+}
+
+async function Run(){
+try {
+    const url = core.getInput('files-added');
+    const repo_token = core.getInput('token');
+    const url_config_token = core.getInput('url-config');
+    // var branch = context.payload.pull_request.head.ref;
+
+    if(url_config && url )
+    {
+      
+      overwriteFile(repo_token,url_config_token)
+
     }
     
     
