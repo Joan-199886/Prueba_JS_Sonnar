416c85231d312d9e5e1877e76b40906a4a90061a
joan-199886 <joan-herrera@upc.edu.co>
2021-07-19 10:25:41 -0500

    prueba 1

--- Mod_Config/index.js
+++ Mod_Config/index.js
@@ -1,24 +1,33 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const { createActionAuth } = require("@octokit/auth-action");
+const { Base64 } = require("js-base64");
 
-
+async function Run(){
 try {
- 
-
     const url = core.getInput('files-added');
-    const token = core.getInput('token').split('');
-    const url_config = core.getInput('url-config').split('');
-    
-    const url2 = url_config.join("");
+    const repotoken = core.getInput('token');
+    const url_config = core.getInput('url-config');
+    // var branch = context.payload.pull_request.head.ref;
 
-    console.log("====================================================");
-    console.log("url: "+url);
-    console.log("url config: "+ url_config.join(""));
-    console.log("token: "+token.join("") );
-    console.log(url2);
+    const {payload: {repository} } = github.context;
+
+    const repoFullName = repository.full_name;
+
+    console.log("RepoFullName:"+repoFullName);
+    
+    if(url_config && url)
+    {
+      console.log("Url_Config : "+url_config.split(""));
+      console.log("Url :"+url.split(""));
+    }
+    
+    
+
     
   } catch (error) {
     core.setFailed(error.message);
   }
 
+}
+Run();
