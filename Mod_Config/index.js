1558beae218d37cacb89727d23fbcfd1bab1c308
unknown <joan-herrera@upc.edu.co>
2021-07-19 20:41:01 -0500

    prueba

--- Mod_Config/index.js
+++ Mod_Config/index.js
@@ -8,9 +8,7 @@ async function overwriteFile(repoToken, pathFile) {
 
   const auth = createActionAuth();
   const authentication = await auth();
-  const octokit = new Octokit({
-    authentication
-  });
+  const octokit = new Octokit({ authentication });
 
  // const octokit = github.getOctokit(repoToken);
 
@@ -19,45 +17,49 @@ async function overwriteFile(repoToken, pathFile) {
   const repoFullName = repository.full_name;
 
   if (repoFullName) {
-    try {
+  
       const [owner, repo] = repoFullName.split("/");
-    const sha = await getSHA(owner, repo, pathFile, octokit);
-    const content = Base64.encode("actualice el documento:" + pathFile);
-    const message = "Actualizacion de datos";
+      const sha = await getSHA(owner, repo, pathFile, octokit);
+      const content = Base64.encode("actualice el documento:");
+      const message = Base64.encode("Actualizacion de datos");
 
-    console.log("pathFile : " + pathFile);
-    console.log("owner : " + owner);
-    console.log("repo : " + repo);
-    console.log("sha : " + sha);
+      const httpResult= await octokit.repos.createOrUpdateFileContents({
+        owner,
+        repo,
+        path: pathFile,
+        message: 'update master.xml',
+        content: content,
+        sha
+        }
+      );
 
-    
-    const httpResult = await octokit.repos.createOrUpdateFileContents({
-      owner,
-      repo,
-      path: "Prueba_JS_Sonnar/Sonnar_Config/config.txt",
-      message: 'update master.xml',
-      content: content,
-      branch: "main",
-      sha
-      }
-    );
-    } catch (error) {
-      core.setFailed(error); 
-    }
-    
+      console.log(httpResult);
+
+  
+
   }
 
 }
 
 async function getSHA(owner, repo, path, octokit) {
-  //const repo_Token = core.getInput('token');
-  //const octokit = github.getOctokit(repo_Token);
-  //const result = await octokit.repos.getContent({ owner, repo, path, });
 
+  // const repo_Token = core.getInput('token');
+  // const octokit = github.getOctokit(repo_Token);
+  
+
+  console.log("pathFile : " + path);
+  console.log("owner : " + owner);
+  console.log("repo : " + repo);
+ 
   const result = await octokit.repos.getContent({ owner, repo, path, });
-
+  
   const sha = result.data.sha;
 
+  // const promise1 = Promise.resolve(result);
+  // promise1.then((value) => {
+  //   console.log(value);
+  //   // expected output: 123
+  //});
   return sha;
 }
 
@@ -68,15 +70,12 @@ async function Run() {
     const url_config_token = core.getInput('url-config').split(";");
     // var branch = context.payload.pull_request.head.ref;
 
+    console.log("REPO : " + repo_token);
     if (url_config_token && url) {
       var url_config = url_config_token[0];
       var url_config = url_config;
       overwriteFile(repo_token, url_config);
-
     }
-
-
-
 
   } catch (error) {
     core.setFailed(error.message);
